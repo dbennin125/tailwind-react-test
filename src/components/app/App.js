223c98c286305b5c86fb8eff594235eb69046b16
Dan B <dbennington125@gmail.com>
2021-08-12 10:38:05 -0700

    router setting up started

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Hello } from '../hello/hello';
+
+
+export const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Hello}/>
+    </Switch>
+   
+  );
+};
